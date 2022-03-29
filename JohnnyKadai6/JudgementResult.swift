9122f3cc998b129964c259a9586b7373afc5a705
0102johnny <rt2824.recruit.1022@gmail.com>
2022-03-29 18:35:36 +0900

    guard文を使って乱数とUISliderのvalueが一致した時、しなかった時の条件分岐の処理を実装 ハードコーディング対策としてAlertのmessageをまとめるenumを追加

---
+++ JohnnyKadai6/JudgementResult.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  JohnnyKadai6
+//
+//  Created by Johnny Toda on 2022/03/29.
+//
+
+import Foundation
+
+enum JudgementResult {
+    static let correct = "あたり!\nあなたの値: "
+    static let incorrect = "はずれ！\nあなたの値: "
+}
--- JohnnyKadai6/ViewController.swift
+++ JohnnyKadai6/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    private let randomValue = Int.random(in: 1...100)
     @IBOutlet private weak var valueLabel: UILabel!
 
     @IBOutlet private weak var slider: UISlider!
@@ -16,17 +17,29 @@ final class ViewController: UIViewController {
     }
 
     @IBAction private func judgeTheResult(_ sender: Any) {
-        showAlert(message: "あなたの値: \(Int(slider.value))")
+        let sliderValue = Int(slider.value)
+
+        guard randomValue == sliderValue else {
+            showAlert(message: JudgementResult.incorrect + "\(sliderValue)")
+            return
+        }
+        showAlert(message: JudgementResult.correct + "\(sliderValue)")
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        valueLabel.text = "\(Int.random(in: 1...100))"
+        valueLabel.text = "\(randomValue)"
     }
+
+
 
     private func showAlert(message: String) {
         let alertController = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "再挑戦", style: .default, handler: nil))
+
+        alertController.addAction(UIAlertAction(title: "再挑戦する", style: .default) { _ in
+            self.viewDidLoad()
+        })
+
         present(alertController, animated: true, completion: nil)
     }
 }
